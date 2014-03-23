485da7bd01bd277822dfee0dc26891f02013747f
Shane Girish <shaneGirish@gmail.com>
2014-03-23 12:50:51 +0000

    Resize all images

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ readImages();
 
 //app.param('size', /^\d+$/);
 
-app.get('/captcha/:UUID/:type', function(req, res){
-	var UUID = req.params.UUID;
-	var type = req.params.type;
-	var body = "false";
+app.get('/captcha/:UUID/:type', function(req, res) {
+    var UUID = req.params.UUID;
+    var type = req.params.type;
+    var body = "false";
 
-	if(answers[UUID] == type) {
-		body = "true";
+    if (answers[UUID] == type) {
+        body = "true";
 
-		delete answers[UUID];
-	}
+        delete answers[UUID];
+    }
 
-	res.setHeader('Content-Type', 'text/json');
-	res.setHeader('Content-Length', Buffer.byteLength(body));
-	res.end(body);
+    res.setHeader('Content-Type', 'text/json');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
 });
 
-app.get('/captcha/:size', function(req, res){
-	var body = JSON.stringify(pickCaptchaSet(req.params.size));
-	res.setHeader('Content-Type', 'text/json');
-	res.setHeader('Content-Length', Buffer.byteLength(body));
-	res.end(body);
+app.get('/captcha/:size', function(req, res) {
+    var body = JSON.stringify(pickCaptchaSet(req.params.size));
+    res.setHeader('Content-Type', 'text/json');
+    res.setHeader('Content-Length', Buffer.byteLength(body));
+    res.end(body);
 });
 
 //app.use(express.methodOverride());
@@ -44,54 +44,56 @@ app.listen(3000);
 console.log('Listening on port 3000');
 
 function readImages() {
-	fs.readdirSync('img').forEach(function(category) {
-		imageDB[category] = [];
-		fs.readdirSync('img' + path.sep + category).forEach(function(image) {
-			var data = fs.readFileSync('img' + path.sep + category + path.sep + image);
-			imageDB[category].push("data:image/" + image.substr(-3) + ";base64," + new Buffer(data).toString('base64'));
-		});
-	});
+    fs.readdirSync('img').forEach(function(category) {
+        imageDB[category] = [];
+        fs.readdirSync('img' + path.sep + category).forEach(function(image) {
+            var data = fs.readFileSync('img' + path.sep + category + path.sep + image);
+            imageDB[category].push("data:image/" + image.substr(-3) + ";base64," + new Buffer(data).toString('base64'));
+        });
+    });
 }
 
 function pickCaptchaSet(size) {
-	var categories  = _.keys(imageDB);
+    var categories = _.keys(imageDB);
 
-	var type = categories[_.random(categories.length - 1)];
-	var image = imageDB[type][_.random(imageDB[type].length - 1)];
-	var UUID = uuid.v4();
+    var type = categories[_.random(categories.length - 1)];
+    var image = imageDB[type][_.random(imageDB[type].length - 1)];
+    var UUID = uuid.v4();
 
-	answers[UUID] = type;
+    answers[UUID] = type;
 
-	var captchaSet = {
-		answer: type
-	};
+    var captchaSet = {
+        answer: type
+    };
 
-	var list = [{
-		UUID: UUID,
-		image: image
-	}];
+    var list = [{
+        UUID: UUID,
+        image: image
+    }];
 
-	while(size > 1) {
-		type = categories[_.random(categories.length - 1)];
-		if(type == captchaSet.answer) {
-			continue;
-		}
+    while (size > 1) {
+        type = categories[_.random(categories.length - 1)];
+        if (type == captchaSet.answer) {
+            continue;
+        }
 
-		image = imageDB[type][_.random(imageDB[type].length - 1)];
+        image = imageDB[type][_.random(imageDB[type].length - 1)];
 
-		if(_.some(list, { 'image': image })) {
-			continue;
-		}
-		
-		list.push({
-			UUID: uuid.v4(),
-			image: image
-		});
+        if (_.some(list, {
+            'image': image
+        })) {
+            continue;
+        }
 
-		size--;
-	}
+        list.push({
+            UUID: uuid.v4(),
+            image: image
+        });
 
-	captchaSet.list = _.shuffle(list);
+        size--;
+    }
 
-	return captchaSet;
+    captchaSet.list = _.shuffle(list);
+
+    return captchaSet;
 }
